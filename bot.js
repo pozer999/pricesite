a4c21bbab8b334c5d6257063826720b0ad24057b
Михаил <101669956+pozer999@users.noreply.github.com>
2025-02-09 17:36:37 +0300

    join debug

--- bot.js
+++ bot.js
@@ -144,7 +144,7 @@ bot.on('callback_query', (query) => {
             try {
                 const cost = calculateCost(pages, type, features);
                 bot.sendMessage(chatId, `Стоимость сайта: ${cost} рублей. \nВы выбрали: \nТип сайта: ${type == "landing" ? "Лендинг" : type == "corporate" ? "Корпоративный" : "Интернет-магазин"} \nКоличество страниц: ${pages}\nДополнительные функции: ${features.join("\n")}`);
-console.log(features);
+console.log(features.join("\n"));
 
                 // Предложить рассчитать снова
                 const options = {
